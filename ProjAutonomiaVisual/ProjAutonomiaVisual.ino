d6b923c02d83f395098a2c6f479092acd4d3206e
Saiony <cauechavesvicente@gmail.com>
2016-05-06 15:21:03 -0300

    Tentativa Varios Sensores

--- ProjAutonomiaVisual/ProjAutonomiaVisual.ino
+++ ProjAutonomiaVisual/ProjAutonomiaVisual.ino
@@ -2,26 +2,30 @@
 
 #define echoPinF 13 //Pino 13 recebe o pulso do echo
 #define trigPinF 12 //Pino 12 envia o pulso para gerar o echo
-#define echoPinD 11 
-#define trigPinD 10 
+#define echoPinD 11 //Pino 11 recebe o pulso do echo
+#define trigPinD 10 //Pino 10 envia o pulso para gerar o echo
 #define echoPinE 9 
 #define trigPinE 8 
 
-#define engine1 2
-#define engine2 3
 
+#define engine1 2 //Define o pino do motor da Esquerda
+#define engine2 3 //Define o pino do motor da Direita
+#define engine3 4 //Define o pino do motor Frontal
+
+#define constante 2
 
 //iniciando a função e passando os pinos
 Ultrasonic ultrasonicF (12,13);
 Ultrasonic ultrasonicD (10,11);
 Ultrasonic ultrasonicE (8,9);
+void giraMotor(char motor, int distancia);
 
-unsigned int dF, dE, dD, sensor;
+unsigned int distanciaFront, distanciaEsq, distanciaDir, sensor;
 
 void setup()
 {
    Serial.begin(9600); //inicia a porta serial
-   pinMode(echoPinF INPUT);   // (recebe)
+   pinMode(echoPinF, INPUT);   // (recebe)
    pinMode(trigPinF, OUTPUT); // (envia)
    pinMode(echoPinE, INPUT);  // (recebe)
    pinMode(trigPinE, OUTPUT); // (envia)
@@ -30,124 +34,61 @@ void setup()
    // define os pinos de saida que vão para o motor
    pinMode(engine1, OUTPUT);
    pinMode(engine2, OUTPUT);
-   dF = dE = dD = 0;
-
+   distanciaFront = distanciaEsq = distanciaDir = 0;
 }
 
 void loop() {
-  leituraSensorF();
-  leituraSensorD();
-  leituraSensorE();
-  if(dE == 0 || dF == 0 || dD == 0)
-    digitalWrite(engine2, LOW);
-    
-  if(distancia >= 150 && distancia < 200){
-      while(distancia >= 150 && distancia < 200){
-        Serial.print("PERTO 200");
-        digitalWrite(engine2, HIGH);
-        delay(MAX/2);
-        digitalWrite(engine2, LOW);
-        delay(MAX/2);
-        leituraSensor();
-      }
-  }
-  else if(distancia >= 100 && distancia < 150){
-      while(distancia > 100 && distancia < 150){
-        Serial.print("PERTO 200");
-        digitalWrite(engine2, HIGH);
-        delay(MAX/3);
-        digitalWrite(engine2, LOW);
-        delay(MAX/3);
-        leituraSensor();
-      }
-  }
-  else if(distancia >= 50 && distancia < 100){
-      while(distancia > 100 && distancia < 150){
-        Serial.print("PERTO 200");
-        digitalWrite(engine2, HIGH);
-        delay(MAX/4);
-        digitalWrite(engine2, LOW);
-        delay(MAX/4);
-        leituraSensor();
-      }
-  }
-  else if(distancia > 0 && distancia < 50){
-      while(distancia > 100 && distancia < 150){
-        Serial.print("PERTO 200");
-        digitalWrite(engine2, HIGH);
-        delay(MAX/6);
-        //digitalWrite(engine2, LOW);
-        //delay(MAX/6);
-        leituraSensor();
-      }
-  }
-  delay(100); // delay de 0,1 segundos
+  distanciaFront = leituraSensor(12, 13);
+  distanciaEsq = leituraSensor(8, 9);
+  distanciaDir = leituraSensor(10, 11);
+
+  giraMotor('f', distanciaFront);
+  giraMotor('d', distanciaDir);
+  giraMotor('e', distanciaEsq);   
 }
 
 // ---------------------------------- LEITURA DO SENSOR FRONTAL ---------------------------------- //
 
-void leituraSensorF(){
-
-    //seta o pino 12 com um pulso baixo "LOW" ou desligado ou ainda 0
+int leituraSensor(int trigPin, int echoPin){
+  float distancia = 0;
+  //seta o pino TRIG com um pulso "LOW"
     digitalWrite(trigPinF, LOW);
   // delay de 2 microssegundos
     delayMicroseconds(2);
-  //seta o pino 12 com pulso alto "HIGH" ou ligado ou ainda 1
+  //seta o pino TRIG com pulso "HIGH"
     digitalWrite(trigPinF, HIGH);
   //delay de 10 microssegundos
     delayMicroseconds(10);
-  //seta o pino 12 com pulso baixo novamente
+  //seta o pino TRIG com pulso "LOW" novamente
     digitalWrite(trigPinF, LOW);
-  // função Ranging, faz a conversão do tempo de
-  //resposta do echo em centimetros, e armazena
-  //na variavel distancia
-    dF = (ultrasonicF.Ranging(CM));
-    Serial.println(dF);
-
+  // função Ranging, faz a conversão do tempo de resposta do echo em centimetros, e armazena na variavel distancia
+    distancia = (ultrasonicF.Ranging(CM));
+    Serial.println(distancia);
+    return distancia;
 }
 
-// ---------------------------------- LEITURA DO SENSOR DA ESQUERDA ---------------------------------- //
+void giraMotor(char motor, int distancia)
+{
+   int qualMotor, tempoDelay = 0;
 
-void leituraSensorE(){
+   //Define qual motor irá girar
+   if(motor == 'f')
+    qualMotor = engine3;
+   else if(motor == 'e')
+    qualMotor = engine1;
+   else if(motor == 'd')
+    qualMotor = engine2;
 
-    //seta o pino 12 com um pulso baixo "LOW" ou desligado ou ainda 0
-    digitalWrite(trigPinE, LOW);
-  // delay de 2 microssegundos
-    delayMicroseconds(2);
-  //seta o pino 12 com pulso alto "HIGH" ou ligado ou ainda 1
-    digitalWrite(trigPinE, HIGH);
-  //delay de 10 microssegundos
-    delayMicroseconds(10);
-  //seta o pino 12 com pulso baixo novamente
-    digitalWrite(trigPinE, LOW);
-  // função Ranging, faz a conversão do tempo de
-  //resposta do echo em centimetros, e armazena
-  //na variavel distancia
-    dE = (ultrasonicE.Ranging(CM));
-    Serial.println(dE);
+   //Calcula o tempo do delay de acordo com a distancia calculada
+   tempoDelay = distancia;
+      
+   if(distancia == 0 || distancia > 300)
+     digitalWrite(qualMotor, LOW);
+    
+   digitalWrite(qualMotor, HIGH);
+   delay(tempoDelay);
 
+   digitalWrite(qualMotor, LOW);
+   delay(tempoDelay);
 }
-
-// ---------------------------------- LEITURA DO SENSOR DA DIRETA ---------------------------------- //
-
-void leituraSensorD(){
-
-    //seta o pino 12 com um pulso baixo "LOW" ou desligado ou ainda 0
-    digitalWrite(trigPinD, LOW);
-  // delay de 2 microssegundos
-    delayMicroseconds(2);
-  //seta o pino 12 com pulso alto "HIGH" ou ligado ou ainda 1
-    digitalWrite(trigPinD, HIGH);
-  //delay de 10 microssegundos
-    delayMicroseconds(10);
-  //seta o pino 12 com pulso baixo novamente
-    digitalWrite(trigPinD, LOW);
-  // função Ranging, faz a conversão do tempo de
-  //resposta do echo em centimetros, e armazena
-  //na variavel distancia
-    dD = (ultrasonicD.Ranging(CM));
-    Serial.println(dD);
-
-}
- 
 
